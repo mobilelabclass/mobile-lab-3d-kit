376b5c5a625de08b779e82eb27b1b43dfd0aaebd
Nien Lam <nien@linebreakdesign.com>
2018-03-08 15:49:22 -0500

    Add files.

---
+++ MobileLab3DKit/ViewController.swift
@@ -0,0 +1,177 @@
+//
+//  ViewController.swift
+//  MobileLab3DKit
+//
+//  Created by Nien Lam on 3/8/18.
+//  Copyright © 2018 Mobile Lab. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+import SpriteKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // create and add a camera to the scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        /*
+        // retrieve the ship node
+        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        
+        // animate the 3d object
+        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+ */
+ 
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.black
+        
+        // add a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        scnView.addGestureRecognizer(tapGesture)
+
+    
+        if let scene = SKScene(fileNamed: "MenuOverlay") as? MenuOverlay {
+            scene.scaleMode = .resizeFill
+            scnView.overlaySKScene = scene
+        }
+    }
+    
+    @objc
+    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+
+        let characterNode = scnView.scene?.rootNode.childNode(withName: "IdleFixed", recursively: true)!
+        let animation = SCNAnimationPlayer.loadAnimation(fromSceneNamed: "art.scnassets/JumpingFixed.dae")
+        characterNode!.addAnimationPlayer(animation, forKey: "animation")
+        animation.play()
+        
+        // check what nodes are tapped
+        let p = gestureRecognize.location(in: scnView)
+        let hitResults = scnView.hitTest(p, options: [:])
+        // check that we clicked on at least one object
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result = hitResults[0]
+            
+            // get its material
+            let material = result.node.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            
+            // on completion - unhighlight
+            SCNTransaction.completionBlock = {
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                
+                material.emission.contents = UIColor.black
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = UIColor.red
+            
+            SCNTransaction.commit()
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    
+//    func loadAnimation(withKey: String, sceneName:String, animationIdentifier:String) {
+//        let sceneURL = Bundle.main.url(forResource: sceneName, withExtension: "dae")
+//        let sceneSource = SCNSceneSource(url: sceneURL!, options: nil)
+//
+//        if let animationObject = sceneSource?.entryWithIdentifier(animationIdentifier, withClass: CAAnimation.self) {
+//            // The animation will only play once
+//            animationObject.repeatCount = 1
+//            // To create smooth transitions between animations
+//            animationObject.fadeInDuration = CGFloat(1)
+//            animationObject.fadeOutDuration = CGFloat(0.5)
+//
+//            // Store the animation for later use
+//            animations[withKey] = animationObject
+//        }
+//    }
+}
+
+
+
+
+// For loading animations with in collada model.
+extension SCNAnimationPlayer {
+    class func loadAnimation(fromSceneNamed sceneName: String) -> SCNAnimationPlayer {
+        let scene = SCNScene( named: sceneName )!
+        // find top level animation
+        var animationPlayer: SCNAnimationPlayer! = nil
+        scene.rootNode.enumerateChildNodes { (child, stop) in
+            if !child.animationKeys.isEmpty {
+                animationPlayer = child.animationPlayer(forKey: child.animationKeys[0])
+                stop.pointee = true
+            }
+        }
+        return animationPlayer
+    }
+}
+
